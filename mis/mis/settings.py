6e1cffd13c69fb889fb4efd35ce6891b31feb134
shahchhatru <shahkhesavchhatra@gmail.com>
2024-08-25 10:54:01 +0545

    redis added

--- mis/mis/settings.py
+++ mis/mis/settings.py
@@ -111,6 +111,17 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+     "TIMEOUT": 300,
+}
+
 # If you are running Django inside a Docker container and the dev-db service is in the same Docker network as the Django service, you should use the service name dev-db as the HOST
 
 
